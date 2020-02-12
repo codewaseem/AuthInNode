ff8a43f18fd313b789182905f7ab0bf97d24164b
Waseem Ahmed <codewaseem@gmail.com>
2020-02-12 22:28:22 +0530

    disable warning

--- src/core/interactors/auth-interactor.ts
+++ src/core/interactors/auth-interactor.ts
@@ -1,4 +1,5 @@
 class AuthInteractor implements AuthController {
+  // eslint-disable-next-line no-unused-vars
   signUp(signUpData: SignUpData): Promise<User> {
     throw new Error("Method not implemented.");
   }
